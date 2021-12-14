7e970006336a35704c5f1285d9b6bd9b5b4c0ec8
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-12-14 16:48:48 +0100

    Changed wrong brackets

--- script/index.js
+++ script/index.js
@@ -6,25 +6,27 @@ window.onload = async function () {
     /////////////////////////
     // ---- MAIN PAGE ---- //
     /////////////////////////
-    let mainPageNumber = 0;
 
-    if (mainPageNumber == 0) {
-        homepageDeals();
-    }
+    if (window.location.pathname == '/index.html') {
+        let mainPageNumber = 0;
 
-    function homepageDeals() {
-        fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1,6,10,12&sortBy=Savings&pageSize=4&pageNumber=${mainPageNumber}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
+        if (mainPageNumber == 0) {
+            homepageDeals();
+        }
 
-                console.log("Main page fetch!", data);
+        function homepageDeals() {
+            fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1,6,10,12&sortBy=Savings&pageSize=4&pageNumber=${mainPageNumber}`)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
 
-                //CHANGE BODY TO RESULTS
-                for (let k of data) {
-                    document.getElementById("body-search-list").innerHTML +=
-                        `
+                    console.log("Main page fetch!", data);
+
+                    //CHANGE BODY TO RESULTS
+                    for (let k of data) {
+                        document.getElementById("body-search-list").innerHTML +=
+                            `
                                     <div class="body-search-list-game">
                                         <div class="body-search-list-game-container1">
                                             <img class="body-search-list-game-thumb" src="${k.thumb}">
@@ -37,17 +39,61 @@ window.onload = async function () {
                                         </div>
                                     </div>
                                     `;
-                }
-            });
+                    }
+                });
+        }
+        let loadMore = document.getElementById("loadMore");
+
+        loadMore.addEventListener("click", e => {
+            mainPageNumber += 1;
+            homepageDeals();
+        });
+
+
+
+        //////////////////////////
+        // ---- SEARCH BOX ---- //
+        //////////////////////////
+
+        document.getElementById("header-search-form").addEventListener("submit", e => {
+            e.preventDefault();
+
+            //Remove load more
+            document.getElementById("body-search-more").innerHTML = ``;
+
+            let searchBox = document.getElementById("header-search-box").value;
+            console.log(searchBox);
+            fetch(`https://www.cheapshark.com/api/1.0/deals?title=${searchBox}&storeID=1&pageSize=20&sortBy=Reviews`)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log("Search fetch!", data);
+
+                    //deletes previous html entries if you search again
+                    document.getElementById("body-search-list").innerHTML = ``
+
+                    //CHANGE BODY TO SEARCH RESULTS
+                    for (let k of data) {
+                        document.getElementById("body-search-list").innerHTML +=
+                            `
+                        <div class="body-search-list-game">
+                            <div class="body-search-list-game-container1">
+                                <img class="body-search-list-game-thumb" src="${k.thumb}">
+                            </div>
+                            <div class="body-search-list-game-container2">
+                                <p>${k.title}</p>
+                                <a href="#">view</a>
+                                <p>${k.normalPrice} $</p>
+                            </div>
+                        </div>
+                        `;
+                    }
+                })
+        });
+
+
     }
-    let loadMore = document.getElementById("loadMore");
-
-    loadMore.addEventListener("click", e => {
-        mainPageNumber += 1;
-        homepageDeals();
-    });
-
-
 
 
     //////////////////////////
@@ -55,7 +101,6 @@ window.onload = async function () {
     //////////////////////////
 
     //Taken from:   https://stackoverflow.com/questions/9578348/best-way-to-execute-js-only-on-specific-page
-
     if (window.location.pathname == '/login.html') {
 
         //SessionStorage   src:   https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage?retiredLocale=nl
@@ -105,49 +150,4 @@ window.onload = async function () {
                 })
         })
     }
-
-
-
-    //////////////////////////
-    // ---- SEARCH BOX ---- //
-    //////////////////////////
-
-    document.getElementById("header-search-form").addEventListener("submit", e => {
-        e.preventDefault();
-
-        //Remove load more
-        document.getElementById("body-search-more").innerHTML = ``;
-
-        let searchBox = document.getElementById("header-search-box").value;
-        console.log(searchBox);
-        fetch(`https://www.cheapshark.com/api/1.0/deals?title=${searchBox}&storeID=1&pageSize=20&sortBy=Reviews`)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log("Search fetch!", data);
-
-                //deletes previous html entries if you search again
-                document.getElementById("body-search-list").innerHTML = ``
-
-                //CHANGE BODY TO SEARCH RESULTS
-                for (let k of data) {
-                    document.getElementById("body-search-list").innerHTML +=
-                        `
-                        <div class="body-search-list-game">
-                            <div class="body-search-list-game-container1">
-                                <img class="body-search-list-game-thumb" src="${k.thumb}">
-                            </div>
-                            <div class="body-search-list-game-container2">
-                                <p>${k.title}</p>
-                                <a href="#">view</a>
-                                <p>${k.normalPrice} $</p>
-                            </div>
-                        </div>
-                        `;
-                }
-            })
-    });
-
-
 }
